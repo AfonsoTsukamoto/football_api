006605093a59c119b673a990e64e1829d6e62253
Bruno Oliveira <bm.oliveira.dev@gmail.com>
2015-07-24 12:26:02 +0100

    refactor event test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -17,20 +17,18 @@ RSpec.describe FootballApi::Event do
     let(:request_response) { FootballApi::Fixture.where(match_date: fixtures_date) }
 
     let(:fixture) {request_response[23]}
-    let(:match) { fixture.match }
-
     let(:local_goal) { fixture.match_events[2] }
     let(:away_goal) { fixture.match_events[0] }
     let(:visitor_red_card) { fixture.match_events[1] }
     let(:local_yellow_card) { fixture.match_events[4] }
 
     describe '.new red_card' do
-      it 'has all values' do
+      it 'not fails' do
         expect(visitor_red_card.event_id).not_to be_nil
         expect(visitor_red_card.event_type).to eq("redcard")
         expect(visitor_red_card.event_team).to eq("visitorteam")
 
-        expect(visitor_red_card.event_match_id).to eq(match.match_id)
+        expect(visitor_red_card.event_match_id).to eq(fixture.match.match_id)
         expect(visitor_red_card.event_minute).not_to be_nil
 
         expect(visitor_red_card.event_player).not_to be_nil
@@ -40,12 +38,12 @@ RSpec.describe FootballApi::Event do
     end
 
     describe '.new yellow_card' do
-      it 'has all values' do
+      it 'not fails' do
         expect(local_yellow_card.event_id).not_to be_nil
         expect(local_yellow_card.event_type).to eq("yellowcard")
         expect(local_yellow_card.event_team).to eq("localteam")
 
-        expect(local_yellow_card.event_match_id).to eq(match.match_id)
+        expect(local_yellow_card.event_match_id).to eq(fixture.match.match_id)
         expect(local_yellow_card.event_minute).not_to be_nil
 
         expect(local_yellow_card.event_player).not_to be_nil
@@ -56,12 +54,12 @@ RSpec.describe FootballApi::Event do
     end
 
     describe '.new local_goal' do
-      it 'has all values' do
+      it 'not fails' do
         expect(local_goal.event_id).not_to be_nil
         expect(local_goal.event_type).to eq("goal")
         expect(local_goal.event_team).to eq("localteam")
 
-        expect(local_goal.event_match_id).to eq(match.match_id)
+        expect(local_goal.event_match_id).to eq(fixture.match.match_id)
         expect(local_goal.event_minute).not_to be_nil
 
         expect(local_goal.event_player).not_to be_nil
@@ -72,12 +70,12 @@ RSpec.describe FootballApi::Event do
     end
 
     describe '.new visitor_goal' do
-      it 'has all values' do
+      it 'not fails' do
         expect(away_goal.event_id).not_to be_nil
         expect(away_goal.event_type).to eq("goal")
         expect(away_goal.event_team).to eq("visitorteam")
 
-        expect(away_goal.event_match_id).to eq(match.match_id)
+        expect(away_goal.event_match_id).to eq(fixture.match.match_id)
         expect(away_goal.event_minute).not_to be_nil
 
         expect(away_goal.event_player).not_to be_nil
